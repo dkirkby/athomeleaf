3f04e7ac10ba412e9260d69c6bcae68996c14cfe
David Kirkby <dkirkby@uci.edu>
2010-05-10 13:54:23 -0700

    Re-arrange config packet so that networkID is first

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
 #define CAPABILITY_LIGHT_FEEDBACK    (1<<1)
 
 typedef struct {
-    unsigned short capabilities;   // a bitmask of user-selectable device capabilities
+    byte networkID; // a short identifier that uniquely identifies us on our local network
+    byte capabilities; // a bitmask of user-selectable device capabilities
     unsigned short temperatureMax; // comfort zone upper limit (degF x 100)
     unsigned short temperatureMin; // comfort zone lower limit (degF x 100)
-    byte networkID; // a short identifier that uniquely identifies us on our local network
 } Config;
 
 #endif
