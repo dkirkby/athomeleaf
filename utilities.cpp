9a68f29570c0a3dd75518bdd60c05232a5ac14af
David Kirkby <dkirkby@uci.edu>
2010-05-11 16:36:07 -0700

    Uncomment cricket chirp data and move into flash memory

--- utilities.cpp
+++ utilities.cpp
@@ -730,8 +730,9 @@ void shiftOut(byte pin,unsigned int ndata,const byte *data) {
 
 #define CRICKET_SAMPLES 413
 
-/**
-byte cricketSample[CRICKET_SAMPLES] = {
+#include <avr/pgmspace.h>
+
+prog_uint8_t cricketSample[CRICKET_SAMPLES] = {
     86, 85, 85, 149, 170, 170, 90, 85, 85, 170, 170, 90, 85, 85, 169, \
     170, 106, 85, 85, 169, 210, 170, 213, 74, 165, 82, 173, 213, 170, \
     148, 82, 181, 214, 170, 146, 82, 213, 218, 170, 74, 82, 89, 219, 90, \
@@ -761,16 +762,11 @@ byte cricketSample[CRICKET_SAMPLES] = {
     85, 149, 170, 170, 86, 85, 85, 170, 170, 90, 85, 85, 170, 170, 106, \
     85, 85, 85, 1
 };
-**/
 
 void cricket(void) {
-    //digitalWrite(RED_LED_PIN,HIGH);
-    /**
     shiftOut(PIEZO_PIN,CRICKET_SAMPLES,cricketSample);
     delay(20);
     shiftOut(PIEZO_PIN,CRICKET_SAMPLES,cricketSample);
     delay(20);
     shiftOut(PIEZO_PIN,CRICKET_SAMPLES,cricketSample);
-    **/
-    //digitalWrite(RED_LED_PIN,LOW);
 }
